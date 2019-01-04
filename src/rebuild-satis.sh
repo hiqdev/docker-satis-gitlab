5bc4a0c15c3ec2202eada5c4e651fdc48ff1a3c3
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2019-01-04 11:58:24 +0200

    Updated to follow mborne/satis-gitlab API changes

--- src/rebuild-satis.sh
+++ src/rebuild-satis.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-./bin/satis-gitlab auth-config \
-    --output config/auth.json \
-    $GITLAB_SITE $GITLAB_TOKEN
-
 ./bin/satis-gitlab gitlab-to-config \
+    --template config/template.json \
     --homepage $SATIS_SITE \
     --output config/satis.json \
-    --additional-config config/prototype.json \
     $GITLAB_SITE $GITLAB_TOKEN
 
 COMPOSER_HOME=$(pwd)/config ./bin/satis-gitlab build config/satis.json web
